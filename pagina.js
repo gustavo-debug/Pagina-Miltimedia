68ec1ba3c1a4b3e4695309b15dfbe22672c7eda7
fcgustavo13 <fcgustavo13@gmail.com>
2020-01-01 16:38:23 -0300

    agregado de los archivos principales

---
+++ pagina.js
@@ -0,0 +1,27 @@
+// JavaScript Document/*
+/*function cambiarNombre(){
+var array;
+array = ['cepillin', 5, "constantine"];
+
+var cost1 = document.getElementById('cost1');
+cost1.textContent = array[0];
+
+ cost1 = document.getElementById("cost2");
+cost1.textContent = array[1];
+
+ cost1 = document.getElementById("cost3");
+cost1.textContent = array[2];
+
+}
+var remplazo1 = document.getElementById("cost1");
+var remplazo2 = document.getElementById("cost2");
+var remplazo3 = document.getElementById("cost3");
+var remplazo4 = document.getElementById("cost4");
+
+remplazo3.onmousedown = cambiarNombre;*/
+
+
+
+
+
+
